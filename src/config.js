ab7fcf8cc64687fa184bb3ae792b916fbe3fef67
mehdad1420 <mehdad.qasemian@gmail.com>
2017-10-22 11:05:24 +0330

    change server

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 //application specific config file
 let config = {};
 
-config.authInstanceId = "59e71ef6e4b0edcca2f82abe";
-config.objectSorageInstanceId = "59e71ef6e4b03ffa02e808f0";
-config.integratedMasterKey = "6b1f1489429a461780dafbfe";
-config.clientKey = "59e71ef6e4b0f840c1225751";
-config.cloudCode = "59e71ef6e4b0edcca2f82ac1";
-config.cdnInstanceId = "59e72024e4b0edcca2f8323f";
+config.authInstanceId = "59ec490de4b03ffa03083698";
+config.objectSorageInstanceId = "59ec490de4b0edcca318e18a";
+config.integratedMasterKey = "c958dce949d44f7699ec77c0";
+config.clientKey = "59ec490de4b05ef67685eff2";
+config.cloudCode = "59ec490ee4b0edcca318e190";
+config.cdnInstanceId = "59ec49e6e4b03ffa03083b09";
 
-config.baseUrl = "http://storage.backtory.com/samira";
+config.baseUrl = "http://storage.backtory.com/samira-imdb";
 
 
 config.lambdaHeaders =
